52959ddde59431320bc72cdc83a721edb604fdbf
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-08-28 11:15:11 -0300

    ex4

---
+++ Time.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstracao
+{
+    internal class Time
+    {
+        public string NomeTime;
+        public List<Jogador> ListaDeJogadores = new List<Jogador>();
+
+        public void AdicionarJogador(Jogador jogador)
+        {
+            ListaDeJogadores.Add(jogador);
+        }
+
+        public void ListarJogadores()
+        {
+            Console.WriteLine($"Time: {NomeTime}");
+            Console.WriteLine("Escalação:");
+
+            foreach (Jogador jogador in ListaDeJogadores)
+            {
+                Console.WriteLine($"Camisa {jogador.NumeroCamisa}-{jogador.Nome}-{jogador.Posicao}");
+            }
+
+        }
+    }
+}
