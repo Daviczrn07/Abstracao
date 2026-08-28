88922f7141dd7bb7c6b50fcf291e3b705cebd895
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-08-28 08:38:02 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Abstracao
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int QuantidadeEmEstoque;
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Nome:{Nome}");
+            Console.WriteLine($"Preco: R$ {Preco}");
+            Console.WriteLine($"Quantidade em estoque: {QuantidadeEmEstoque}");
+            Console.WriteLine("-----------------------");
+        }
+    }
+
+
+}
+
